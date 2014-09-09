8efbf0b3bd6b275e8e75b461fe2bf4a21b4f6851
jyy <njujiangyy@gmail.com>
2014-09-09 18:30:03 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  18:30:03 up 5 min,  2 users,  load average: 0.12, 0.08, 0.04 init-+-acpid      |-cron      |-2*[dhclient]      |-exim4      |-5*[getty]      |-login---bash      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

---
+++ src/elf/loader.d
@@ -0,0 +1,1 @@
+src/elf/loader.o: src/elf/loader.c
---
+++ src/exec/exec.d
@@ -0,0 +1,24 @@
+src/exec/exec.o: src/exec/exec.c include/exec/helper.h include/cpu/reg.h \
+ include/common.h include/debug.h /usr/include/stdio.h \
+ /usr/include/features.h /usr/include/i386-linux-gnu/bits/predefs.h \
+ /usr/include/i386-linux-gnu/sys/cdefs.h \
+ /usr/include/i386-linux-gnu/bits/wordsize.h \
+ /usr/include/i386-linux-gnu/gnu/stubs.h \
+ /usr/include/i386-linux-gnu/gnu/stubs-32.h \
+ /usr/lib/gcc/i486-linux-gnu/4.7/include/stddef.h \
+ /usr/include/i386-linux-gnu/bits/types.h \
+ /usr/include/i386-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/i486-linux-gnu/4.7/include/stdarg.h \
+ /usr/include/i386-linux-gnu/bits/stdio_lim.h \
+ /usr/include/i386-linux-gnu/bits/sys_errlist.h \
+ /usr/include/i386-linux-gnu/bits/stdio.h /usr/include/assert.h \
+ include/macro.h /usr/lib/gcc/i486-linux-gnu/4.7/include/stdint.h \
+ /usr/include/stdint.h /usr/include/i386-linux-gnu/bits/wchar.h \
+ /usr/include/string.h /usr/include/xlocale.h \
+ /usr/include/i386-linux-gnu/bits/string.h \
+ /usr/include/i386-linux-gnu/bits/string2.h /usr/include/endian.h \
+ /usr/include/i386-linux-gnu/bits/endian.h \
+ /usr/include/i386-linux-gnu/bits/byteswap.h /usr/include/stdlib.h \
+ include/memory.h include/common.h src/exec/all-instr.h \
+ src/exec/prefix/prefix.h src/exec/data-mov/mov.h src/exec/misc/misc.h
