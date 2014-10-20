6d0bd0d7ec8e141ac15e4729ee7df086c6f1f60f
jyy <njujiangyy@gmail.com>
2014-10-20 15:26:12 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  15:26:12 up  1:30,  1 user,  load average: 0.03, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---tmux      |-tmux-+-bash---less      |      |-bash---make---sh-+-git      |      |                  `-pstree      |      |-2*[bash---vim]      |      `-bash---gdb      `-udevd---2*[udevd]

--- src/exec/ctrl-trans/ret.c
+++ src/exec/ctrl-trans/ret.c
@@ -4,11 +4,11 @@ extern char suffix;
 
 make_helper(ret) {
 	if(suffix == 'l') {
-		cpu.eip = swaddr_read(reg_l(R_ESP), 4);
+		cpu.eip = swaddr_read(reg_l(R_ESP), 4) - 1;
 		reg_l(R_ESP) += 4;
 	}
 	else {
-		cpu.eip = swaddr_read(reg_l(R_ESP), 2) & 0xffff;
+		cpu.eip = (swaddr_read(reg_l(R_ESP), 2) & 0xffff) - 1;
 		reg_l(R_ESP) += 2;
 	}
 
