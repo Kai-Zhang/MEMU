c73fce4e78495ccf0563188b1ffe07e8fd22c8e0
jyy <njujiangyy@gmail.com>
2014-10-21 14:13:45 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.57-3 i686 GNU/Linux  14:13:45 up  2:44,  1 user,  load average: 0.08, 0.04, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---tmux      |-tmux---bash---vim---bash-+-make---sh-+-git      |                          |           `-pstree      |                          `-tee      `-udevd---2*[udevd]

---
+++ src/exec/str-trans/rep.c
@@ -0,0 +1,29 @@
+#include "exec/helper.h"
+#include "movs.h"
+
+extern char suffix;
+extern int exec(swaddr_t);
+
+make_helper(rep) {
+	int len = 0;
+	if(!reg_l(R_ECX)) {
+		switch(swaddr_read(eip + 1, 1)) {
+			case 0xa4:
+				print_asm("rep movsb");				break;
+			case 0xa5:
+				print_asm("rep movs%c", suffix);	break;
+			default:	
+					printf("Undefined rep operator(eip = 0x%08x)\n", cpu.eip);
+					assert(0);
+		};
+	}
+	else {
+		for (; reg_l(R_ECX) > 0; --reg_l(R_ECX)) {
+			len = exec(eip + 1);
+		}
+		static char rep_prim[40];
+		strncpy(rep_prim, assembly, 40);
+		print_asm("rep %s", rep_prim);
+	}
+	return 1 + len;
+}
