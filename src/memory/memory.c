b5b086a0deb7ef50b8602da87aed7e691f63b166
john <j.zhangkai1994@gmail.com>
2015-01-26 18:08:33 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.63-2 i686 GNU/Linux  18:08:33 up 54 min,  1 user,  load average: 0.19, 0.12, 0.06 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -8,13 +8,12 @@ void dram_write(hwaddr_t addr, size_t len, uint32_t data);
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);//cache_read(addr, len);
+	return cache_read(addr, len);
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-	//cache_write(addr, len, data);
-	dram_write(addr, len, data);
+	cache_write(addr, len, data);
 }
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
@@ -29,7 +28,7 @@ void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
 
 static uint32_t hwaddr_read_instr(hwaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
-	return dram_read(addr, len);//cache_read(addr, len);
+	return cache_read(addr, len);
 }
 
 uint32_t instr_fetch(swaddr_t addr, size_t len) {
