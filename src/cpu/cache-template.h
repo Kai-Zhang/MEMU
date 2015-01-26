d97e75b123d8e718c66454bbc99fc111ea31e5ab
john <j.zhangkai1994@gmail.com>
2015-01-26 20:21:09 +0800

    > compile Linux debian 3.2.0-4-486 #1 Debian 3.2.63-2 i686 GNU/Linux  20:21:09 up  3:07,  1 user,  load average: 0.02, 0.03, 0.05 init-+-VBoxService---7*[{VBoxService}]      |-acpid      |-cron      |-2*[dhclient]      |-exim4      |-6*[getty]      |-rsyslogd---3*[{rsyslogd}]      |-sshd---sshd---sshd---bash---make---sh-+-git      |                                       `-pstree      `-udevd---2*[udevd]

--- src/cpu/cache-template.h
+++ src/cpu/cache-template.h
@@ -135,7 +135,6 @@ static void concat(concat(__, CACHE_NAME), _read)(hwaddr_t addr, void *data) {
 		}
 		if (!CACHE_NAME[set][i].valid) {
 			replaced = i;
-			break;
 		}
 	}
 	if (i == NR_WAY) {
@@ -165,7 +164,6 @@ static void concat(concat(__, CACHE_NAME), _write)(hwaddr_t addr, void *data, ui
 #ifdef WRITE_ALLOCATE
 		if (!CACHE_NAME[set][i].valid) {
 			replaced = i;
-			break;
 		}
 #endif
 	}
